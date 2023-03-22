4ee114a84b14295a0ff2f8f86a055223bf13140a
18ebutzler1 <123682836+18ebutzler1@users.noreply.github.com>
2023-03-22 15:52:32 -0400

    Add files via upload

--- activities/Emma_Butzler.py
+++ activities/Emma_Butzler.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+
 import os
 
 import grass.script as gs
@@ -25,6 +25,14 @@ def run_contours(scanned_elev, env, **kwargs):
         env=env,
     )
 
+def run_curvatures(scanned_elev, env, **kwargs):
+    gs.run_command('r.param.scale', input=scanned_elev, output='profile_curv',
+                   method='profc', size=11, env=env)
+    gs.run_command('r.param.scale', input=scanned_elev, output='tangential_curv',
+                   method='crosc', size=11, env=env)
+    gs.run_command('r.colors', map=['profile_curv', 'tangential_curv'], color='curvature', env=env)
+    
+
 def main():
     env = os.environ.copy()
     env["GRASS_OVERWRITE"] = "1"
@@ -35,6 +43,7 @@ def main():
 
     run_relief(scanned_elev=elev_resampled, env=env)
     run_fill(scanned_elev=elev_resampled, env=env)
+    run_curvatures(scanned_elev=elev_resampled,env=env)
 
 
 if __name__ == "__main__":
