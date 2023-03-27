4816b0e47f451e2e55e7e385b688b01cdf4a07db
Truffaut Harper <116755683+truffaut-harper@users.noreply.github.com>
2023-03-27 13:52:22 -0400

    Add files via upload

---
+++ activities/truffaut_harper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+
+import grass.script as gs
+
+
+def run_flow(scanned_elev, env, **kwargs):
+    gs.run_command("r.flow",
+                   elevation=scanned_elev,
+                   flowaccumulation="flowaccumulation",
+                   env=env)
+
+
+def main():
+    env = os.environ.copy()
+    env["GRASS_OVERWRITE"] = "1"
+    elevation = "elev_lid792_1m"
+    elev_resampled = "elev_resampled"
+    gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
+    gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
+
+    run_flow(scanned_elev=elev_resampled, env=env)
+
+
+if __name__ == "__main__":
+    main()
