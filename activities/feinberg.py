4c845d5c099d9084740b4ffdd2030b1e10e9b3e9
MarkFeinberg <mfeinbe@ncsu.edu>
2023-03-30 16:23:47 -0400

    black formatting feinberg.py

--- activities/feinberg.py
+++ activities/feinberg.py
@@ -7,7 +7,7 @@ import grass.script as gs
 
 # def run_slope(scanned_elev, env, **kwargs):
 #     gs.run_command("r.slope.aspect", elevation=scanned_elev, slope="slope", env=env)
-    
+
 # def run_lake(scanned_elev, env, **kwargs):
 #     coordinates = [638830, 220150]
 #     gs.run_command('r.lake', elevation=scanned_elev, lake='output_lake',
@@ -16,22 +16,30 @@ import grass.script as gs
 from grass.pygrass.vector import VectorTopo
 from grass.pygrass.vector.geometry import Point
 
+
 def create_vector(name, coordinates):
     my_points = VectorTopo(name)
-    my_points.open(mode='w')
+    my_points.open(mode="w")
     point1 = Point(coordinates[0], coordinates[1])
     my_points.write(point1)
     my_points.close()
 
-    
+
 def run_lake(scanned_elev, env, **kwargs):
     coordinates = [638830, 220150]
     res = gs.raster_what(map=scanned_elev, coord=[coordinates])
-    elev_value = float(res[0][scanned_elev]['value'])
-    gs.run_command('r.lake', elevation=scanned_elev, lake='output_lake',
-                   coordinates=coordinates, water_level=elev_value + 5, env=env)
+    elev_value = float(res[0][scanned_elev]["value"])
+    gs.run_command(
+        "r.lake",
+        elevation=scanned_elev,
+        lake="output_lake",
+        coordinates=coordinates,
+        water_level=elev_value + 5,
+        env=env,
+    )
     create_vector(name="source", coordinates=coordinates)
-    
+
+
 def main():
     env = os.environ.copy()
     env["GRASS_OVERWRITE"] = "1"
