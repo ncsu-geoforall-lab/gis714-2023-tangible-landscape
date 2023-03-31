063b0116a2d40c092bfccf438d6a38eb46ad6e1a
MarkFeinberg <mfeinbe@ncsu.edu>
2023-03-31 12:08:32 -0400

    variable updated to scan for feinberg.py

--- activities/feinberg.py
+++ activities/feinberg.py
@@ -42,11 +42,11 @@ def main():
     env = os.environ.copy()
     env["GRASS_OVERWRITE"] = "1"
     elevation = "elev_lid792_1m"
-    elev_resampled = "elev_resampled"
+    scan = "elev_resampled"
     gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
-    gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
+    gs.run_command("r.resamp.stats", input=elevation, output=scan, env=env)
 
-    run_lake(scanned_elev=elev_resampled, env=env)
+    run_lake(scanned_elev=scan, env=env)
 
 
 if __name__ == "__main__":
