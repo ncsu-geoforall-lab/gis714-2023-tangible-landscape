9cc4164ddedf5072aa11e2def89d177e0fdea0d4
ocsmit <osmith2@ncsu.edu>
2023-04-03 15:22:00 -0400

    80 char limit

--- activities/owen.py
+++ activities/owen.py
@@ -1,27 +1,11 @@
 #!/usr/bin/env python3
 
-"""
-Instructions
-
-- Functions intended to run for each scan
-  need to start with `run_`, e.g., `run_slope`.
-
-- Do not modify the parameters of the `run_` function
-  unless you know what you are doing.
-  See optional parameters at:
-  https://github.com/tangible-landscape/grass-tangible-landscape/wiki/Running-analyses-and-developing-workflows#python-workflows
-
-- All gs.run_command/read_command/write_command/parse_command
-  need to be passed *env* parameter like this: `(..., env=env)`.
-"""
-
 import os
 
 import grass.script as gs
 
 
 def run_drain(scanned_elev, env, **kwargs):
-    # r.watershed -sa elevation=elevation accumulation=flow_accum drainage=drain_directions
     gs.run_command(
         "r.watershed",
         elevation=scanned_elev,
@@ -31,10 +15,6 @@ def run_drain(scanned_elev, env, **kwargs):
     )
 
     gs.run_command("r.colors", map="drain_directions", color="plasma", env=env)
-
-
-# calculate flow accumulation using r.accumulate
-# r.accumulate direction=drain_directions accumulation=flow_accum_new
 
 
 def main():
@@ -49,7 +29,12 @@ def main():
     elev_resampled = "elev_resampled"
     # We use resampling to get a similar resolution as with Tangible Landscape.
     gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
-    gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
+    gs.run_command(
+        "r.resamp.stats",
+        input=elevation,
+        output=elev_resampled,
+        env=env,
+    )
     # The end of the block which needs no editing.
 
     # Edit here:
