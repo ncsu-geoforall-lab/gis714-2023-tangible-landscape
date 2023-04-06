314bf5ed4a89de2a1ac6321452a363ca7cff2d5f
cperella <cdperell@ncsu.edu>
2023-04-06 16:16:31 -0400

    black formatting

--- activities/perella_tl.py
+++ activities/perella_tl.py
@@ -6,16 +6,30 @@ import grass.script as gs
 
 
 def run_lcp(scanned_elev, start_coordinate, end_coordinate, env, **kwargs):
-    gs.run_command('r.slope.aspect', elevation=scanned_elev, slope='slope', env=env)
-    gs.run_command('r.cost', input='slope', output='cost', start_coordinates=start_coordinate,
-                   outdir='outdir', flags='k', env=env)
-    gs.run_command('r.colors', map='cost', color='gyr', env=env)
-    gs.run_command('r.drain', input='cost', output='drain', direction='outdir',
-                   drain='drain', flags='d', start_coordinates=end_coordinate, env=env)
+    gs.run_command("r.slope.aspect", elevation=scanned_elev, slope="slope", env=env)
+    gs.run_command(
+        "r.cost",
+        input="slope",
+        output="cost",
+        start_coordinates=start_coordinate,
+        outdir="outdir",
+        flags="k",
+        env=env,
+    )
+    gs.run_command("r.colors", map="cost", color="gyr", env=env)
+    gs.run_command(
+        "r.drain",
+        input="cost",
+        output="drain",
+        direction="outdir",
+        drain="drain",
+        flags="d",
+        start_coordinates=end_coordinate,
+        env=env,
+    )
 
 
 def main():
-
     env = os.environ.copy()
     env["GRASS_OVERWRITE"] = "1"
     elevation = "elev_lid792_1m"
@@ -25,8 +39,13 @@ def main():
     end = [638928, 220472]
     gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
     gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
-    
-    run_lcp(scanned_elev=elev_resampled, start_coordinate=start, end_coordinate=end, env=None)
+
+    run_lcp(
+        scanned_elev=elev_resampled,
+        start_coordinate=start,
+        end_coordinate=end,
+        env=None,
+    )
 
 
 if __name__ == "__main__":
