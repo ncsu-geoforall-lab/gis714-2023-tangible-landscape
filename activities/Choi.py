6a7aca7e47244901b5f6c7fc49b2f5e46377ae72
Grace Choi <112022758+graceeunhyechoi@users.noreply.github.com>
2023-04-06 15:36:52 -0400

    Add files via upload

---
+++ activities/Choi.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+
+import grass.script as gs
+
+
+def run_hydro(scanned_elev, env, **kwargs):
+    gs.run_command(
+        "r.watershed",
+        elevation=scanned_elev,
+        accumulation="flow_accum",
+        basin="watersheds",
+        threshold=1000,
+        flags="a",
+        env=env,
+    )
+
+
+def main():
+    env = os.environ.copy()
+    env["GRASS_OVERWRITE"] = "1"
+    elevation = "elev_lid792_1m"
+    elev_resampled = "elev_resampled"
+    gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
+    gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
+
+    run_hydro(scanned_elev=elev_resampled, env=env)
+
+
+if __name__ == "__main__":
+    main()
