eb5551a153ee632011a2dcf393e5f92ae1408cf6
jhuerta99 <118194644+jhuerta99@users.noreply.github.com>
2023-04-11 22:14:42 -0400

    Update huerta.py

--- activities/huerta.py
+++ activities/huerta.py
@@ -6,15 +6,17 @@ import grass.script as gs
 
 
 def run_hydro(scanned_elev, env, **kwargs):
-    gs.run_command(
-        "r.watershed",
-        elevation=scanned_elev,
-        accumulation="flow_accum",
-        basin="watersheds",
-        threshold=1000,
-        flags="a",
-        env=env,
-    )
+    gs.run_command('r.watershed', elevation=scanned_elev, accumulation='flow_accum',
+                   basin='watersheds', threshold=1000, flags='a', env=env)
+    gs.run_command('r.to.vect', input='watersheds', output='watersheds', type='area', env=env)
+
+def run_watershed_slope(scanned_elev, env, **kwargs):
+    gs.run_command('r.watershed', elevation=scanned_elev, accumulation='flow_accum',
+                   basin='watersheds', threshold=1000, env=env)
+    gs.run_command('r.slope.aspect', elevation=scanned_elev, slope='slope', env=env)
+    gs.run_command('r.stats.zonal', base='watersheds', cover='slope', method='average',
+                   output='watersheds_slope', env=env)
+    gs.run_command('r.colors', map='watersheds_slope', color='bgyr', env=env)
 
 
 def main():
