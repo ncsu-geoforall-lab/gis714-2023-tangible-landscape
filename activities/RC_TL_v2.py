df07d5d86df23c32ab78ce7e4fd99f899e8222d9
Rebecca Composto <35494473+rlwcomposto@users.noreply.github.com>
2023-04-14 09:41:34 -0400

    Add TL files  with r.lake and r.stream.extract (#1)
    These files can be used to find lakes in the tangible landscape (TL) using the elevation in the tangible landscape. When the TL is manipulated, these files can be run and the updated lakes will be shown in the TL. The function came from the GRASS GIS wikipage - https://grasswiki.osgeo.org/wiki/Using_GRASS_GIS_through_Python_and_tangible_interfaces_(workshop_at_FOSS4G_NA_2016)#Tangible_Landscape.

---
+++ activities/RC_TL_v2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import os
+
+import grass.script as gs
+
+
+# Add in new command for the TL to do
+# Rename function
+def run_the_lake(scanned_elev, env, **kwargs):
+    coordinates = [638830, 220150]
+    gs.run_command(
+        "r.lake",
+        elevation=scanned_elev,
+        lake="output_lake",
+        coordinates=coordinates,
+        water_level=120,
+        env=env,
+    )
+    gs.run_command("r.colors", map="output_lake", color="blues", env=env)
+
+
+def run_the_stream(scanned_elev, env, **kwargs):
+    gs.run_command(
+        "r.stream.extract",
+        elevation=scanned_elev,
+        threshold=100,
+        stream_rast="output_stream",
+        env=env,
+    )
+    gs.run_command("r.colors", map="output_stream", color="blues", env=env)
+
+
+def main():
+    env = os.environ.copy()
+    env["GRASS_OVERWRITE"] = "1"
+    elevation = "elev_lid792_1m"
+    elev_resampled = "elev_resampled"
+    gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
+    gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
+
+    run_the_lake(scanned_elev=elev_resampled, env=env)
+    run_the_stream(scanned_elev=elev_resampled, env=env)
+
+
+if __name__ == "__main__":
+    main()
